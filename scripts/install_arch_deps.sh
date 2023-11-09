3ddf3db6bf0a649ce8db59ef46f52b4e85796422
Jariullah Safi <safijari@isu.edu>
2023-11-08 19:00:37 -0600

    Split dep installation into script

---
+++ scripts/install_arch_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+pacman -S --noconfirm git meson clang glslang libcap wlroots \
+  sdl2 vulkan-headers libx11 libxmu libxcomposite libxrender libxres \
+  libxtst libxkbcommon libdrm libinput wayland-protocols benchmark \
+  xorg-xwayland pipewire cmake
