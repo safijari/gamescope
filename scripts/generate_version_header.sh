a1b369e666fcd5b31cb873df093e9385b18bb5a1
Jariullah Safi <safijari@isu.edu>
2023-11-10 13:57:00 -0600

    Add an environment variable to inject a version and a fallback message

--- scripts/generate_version_header.sh
+++ scripts/generate_version_header.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-export VERSION=`git describe --dirty --broken`
+if [[ -z "${GAMESCOPE_VERSION}" ]]; then
+  if git status &>/dev/null; then
+      export VERSION=`git describe --dirty --broken`
+  else
+      export VERSION="Check your package manager for version"
+  fi
+else
+  export VERSION=${GAMESCOPE_VERSION}
+fi
 echo "#define GAMESCOPE_VERSION \"$VERSION\"" > $1
