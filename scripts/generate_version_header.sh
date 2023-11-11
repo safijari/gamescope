88e056c193f99cd3eed4318f1824e31d3a450e29
Jariullah Safi <safijari@isu.edu>
2023-11-10 22:21:18 -0600

    Change header for scripts to #!/bin/sh

--- scripts/generate_version_header.sh
+++ scripts/generate_version_header.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 if [[ -z "${GAMESCOPE_VERSION}" ]]; then
   if git status &>/dev/null; then
       export VERSION=`git describe --dirty --broken`
   else
-      export VERSION="Check your package manager for version"
+      export VERSION="Check your package manager for the version"
   fi
 else
   export VERSION=${GAMESCOPE_VERSION}
--- scripts/install_arch_deps.sh
+++ scripts/install_arch_deps.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 pacman -S --noconfirm git meson clang glslang libcap wlroots \
   sdl2 vulkan-headers libx11 libxmu libxcomposite libxrender libxres \
   libxtst libxkbcommon libdrm libinput wayland-protocols benchmark \
